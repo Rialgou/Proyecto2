c5bb835293bbc521935fee0500a9022b4939c905
ikarus1452 <victorj.mora@hotmail.com>
2021-07-25 18:53:29 -0400

    main con medicion de tiempo promedio

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include "MapSV.h"
 #include "MapAVL.h"
+#include "MapH.h"
 using namespace std;
 string random_string( size_t length )
 {
@@ -14,6 +15,91 @@ string random_string( size_t length )
     generate_n( str.begin(), length, randchar );
     return str;
 }
+
+
+int main(){
+    int n;
+    cout<<"ingrese la cantidad de claves"<<endl;
+    cin >>n;
+    int rep =1;
+    clock_t start,stop;
+    srand(time(NULL));
+    //at1=busqueda de elemento existente, at2 busqueda de elemento no existente
+    double AVLins=0,AVLat1=0,AVLat2=0;
+    double MapHins=0,MapHat1=0,MapHat2=0;
+    double SVins=0,SVat1=0,SVat2=0;
+
+    for(int i=0;i<rep;i++){
+        MapH *h = new MapH(n);//hash
+        MapAVL *a = new MapAVL();//AVL tree
+        MapSV *sv = new MapSV();//sorted vector
+        string s[n];
+        for(int j=0;j<n;j++){
+            s[j]= random_string(1+rand()%10);
+            while(s[j]=="hola") s[j]= random_string(1+rand()%10);
+            pair<string,int> p=make_pair(s[j],rand()%3000);
+
+            start = clock();
+            h->insert(p);
+            stop = clock();
+            MapHins+=(stop-start)/CLOCKS_PER_SEC;
+            
+            start = clock();
+            a->insert(p);
+            stop = clock();
+            AVLins+=(stop-start)/CLOCKS_PER_SEC;
+
+            start = clock();
+            sv->insert(p);
+            stop = clock();
+            SVins+=(stop-start)/CLOCKS_PER_SEC;
+        }
+        for(int j=0;j<n;j++){
+            start = clock();
+            h->at(s[j]);
+            stop = clock();
+            MapHat1+=(stop-start)/CLOCKS_PER_SEC;
+
+            start = clock();
+            h->at("hola");
+            stop = clock();
+            MapHat2+=(stop-start)/CLOCKS_PER_SEC;
+            
+            start = clock();
+            a->at(s[j]);
+            stop = clock();
+            AVLat1+=(stop-start)/CLOCKS_PER_SEC;
+
+            start = clock();
+            a->at("hola");
+            stop = clock();
+            AVLat2+=(stop-start)/CLOCKS_PER_SEC;
+            
+            start = clock();
+            sv->at(s[j]);
+            stop = clock();
+            SVat1+=(stop-start)/CLOCKS_PER_SEC;   
+
+            start = clock();
+            sv->at("hola");
+            stop = clock();
+            SVat2+=(stop-start)/CLOCKS_PER_SEC;   
+        }
+    }
+    cout<<endl;
+    printf("MapH insert : %f\n\n",MapHins/rep );
+    printf("MapH at 1 : %f\n\n",MapHat1/rep );    
+    printf("MapH at 2 : %f\n\n",MapHat2/rep );
+
+    printf("AVL insert : %f\n\n",AVLins/rep );
+    printf("AVL at 1 : %f\n\n",AVLat1/rep );    
+    printf("AVL at 2 : %f\n\n",AVLat2/rep );
+
+    printf("SV insert : %f\n\n",SVins/rep );
+    printf("SV at 1 : %f\n\n",SVat1/rep );    
+    printf("SV at 2 : %f\n\n",SVat2/rep );
+}
+/*
 int main()
 {
     srand(time(NULL));
@@ -30,7 +116,7 @@ int main()
     /*for(int i=0;i<1000;i++){
         sv->insert(make_pair(random_string(5),rand()%100+1));
 
-    }*/
+    }
    /* sv->erase("ola");
     sv->insert(make_pair("ola",22));
     sv->insert(make_pair("ola",25));
@@ -42,7 +128,7 @@ int main()
     else cout<<"no es menor"<<endl<<endl;
     cout << sv->at("ola")<<endl;
     cout << sv->at("juan")<<endl;
-    cout << sv->at("caupolican")<<endl;*/
+    cout << sv->at("caupolican")<<endl;
     MapAVL * avl = new MapAVL();
     avl->insert(make_pair("c",23));
     avl->insert(make_pair("a",213));
@@ -61,3 +147,4 @@ int main()
     cout<<"el valor del string buscado es: "<< avl->at(s)<<endl;
     delete(avl);
 }
+*/
