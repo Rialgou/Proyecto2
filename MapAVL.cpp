a31c10a990aca944b677ba92e0b4dfd18f063441
rialgou <rialgou@gmail.com>
2021-07-26 22:47:57 -0400

    Comentarios en casi todo, falta erase

--- MapAVL.cpp
+++ MapAVL.cpp
@@ -10,45 +10,70 @@ MapAVL::~MapAVL(){
 	raiz = NULL;
 }
 void MapAVL::destructorRec(Nodo * r){
+		//si el nodo es vacio se retorna
     if (r == NULL){
 		return;
     } 
+    //hacemos un recorrido postorder eliminando cada nodo en esa dispocision
     destructorRec(r -> izquierdo);
     destructorRec(r -> derecho);
     delete r;
     r = NULL;
 }
 void MapAVL::insert(pair<string,int> p){
+	//creamos un nodo nuevo
 	Nodo * nuevo = new Nodo(make_pair(p.first,p.second));
+	//utilizamos nuestra funcion recursiva de insertar
 	raiz = insertRec(raiz, nuevo);
 }
 Nodo * MapAVL::insertRec(Nodo * r,Nodo * nuevo){
+	//si no hay raiz, se crea
 	if(r == NULL){
 		r = nuevo;
 		//cout<<"se ingreso el nodo raiz"<<endl;
 		return r;
 	}
+	//si la clave del nodo nuevo es igual a la clave en el nivel, se retorna la raiz
 	else if(nuevo->clave == r->clave){
 		//cout<<"el elemento esta repetido"<<endl;
 		return r;
 	}
+	//si la clave nueva es menor que la clave del nivel, bajamos por la rama izquierda
 	else if(abcMenor(nuevo->clave,r->clave)){
 		r->izquierdo = insertRec(r->izquierdo,nuevo);
 	} 
+	//si la clave nueva es mayor que la clave del nivel, bajamos por la rama derecha
 	else if(!abcMenor(nuevo->clave,r->clave)){
 		r->derecho = insertRec(r->derecho,nuevo);
 	}
+	//hasta este momento hacemos el recorrido normal de un BST, ahora viene la parte del
+	//AVL
+	//sacamos el factor de balance, que no es más que calcular la altura del subarbol izquierdo
+	//menos la altura del subarbol derecho, esto nos ayudara a determinar el subarbol más
+	//grande (bf>1 el hijo izquierdo tiene mayor altura, bf<-1 el hijo derecho tiene mayor altura)
 	int bf = getBalanceFactor(r);
+	//si el factor de balance es mayor que 1 y la clave del nuevo nodo ingresada 
+	//es menor que la clave del hijo izquierdo del nodo r en ese momento 
+	//entramos en este caso zig-zig, se aplica rotacion derecha 
 	if(bf>1 && abcMenor(nuevo->clave,r->izquierdo->clave)){
 		return rightRotate(r);
 	}
+	//si el factor de balance es menor que -1 y la clave del nuevo nodo ingresada 
+	//es mayor que la clave del hijo derecho del nodo r en ese momento 
+	//entramos en este caso zig-zig, se aplica rotacion izquierda
 	if(bf<-1 && !abcMenor(nuevo->clave,r->derecho->clave)){
 		return leftRotate(r);
 	}
+	//si el factor de balance es mayor que 1 y la clave del nuevo nodo ingresada 
+	//es mayor que la clave del hijo izquierdo del nodo r en ese momento 
+	//entramos en este caso zig-zag  
 	if(bf >1 && !abcMenor(nuevo->clave,r->izquierdo->clave)){
 		r->izquierdo = leftRotate(r->izquierdo);
 		return rightRotate(r);
 	}
+	//si el factor de balance es menor que -1 y la clave del nuevo nodo ingresada 
+	//es menor que la clave del hijo derecho del nodo r en ese momento 
+	//entramos en este caso zig-zag 
 	if(bf<-1 && abcMenor(nuevo->clave,r->derecho->clave)){
 		r->derecho = rightRotate(r->derecho);
 		return leftRotate(r);
@@ -56,6 +81,7 @@ Nodo * MapAVL::insertRec(Nodo * r,Nodo * nuevo){
 	return r;
 }
 void MapAVL::erase(string s){
+	//llama al metodo recursivo de erase
 	raiz = eraseRec(raiz,s);
 }
 Nodo * MapAVL::eraseRec(Nodo * r,string s){
@@ -89,23 +115,27 @@ Nodo * MapAVL::eraseRec(Nodo * r,string s){
     return r;
 }
 int MapAVL::at(string s){
+	//llama al metodo recursivo de at
 	Nodo * numero = atRec(raiz,s);
+	//si el nodo devuelto no es nulo, devolvemos el valor de ese nodo
 	if(numero!=NULL){
 		return numero->valor;
 	}
+	//si el nodo devuelto es nulo, devolvemos INT_MIN
 	else return INT_MIN;
 }
 Nodo * MapAVL::atRec(Nodo* r,string s){
+	//si se encuentra la clave o llegamos a una rama, se retorna r
 	if (r == NULL || r -> clave == s)
       return r;
-
-    else if (abcMenor(s, r->clave)) {
+  //si la clave a buscar es menor que la clave del nodo actual, se busca hacia el hijo izquierdo
+  else if (abcMenor(s, r->clave)) {
       return atRec(r->izquierdo , s);
-    }
-    else{
+  }
+  //si la clave a buscar es mayor que la clave del nodo actual, se busca hacia el hijo derecho
+  else{
       return atRec(r->derecho, s);
-    }
-    return r;
+  }
 }
 int MapAVL::size(){
 	return 0;
@@ -128,37 +158,47 @@ void MapAVL::printAVL(Nodo *r,int space){
 	printAVL(r->izquierdo,space);
 }
 int MapAVL::getBalanceFactor(Nodo * n){
+	//si el nodo es hoja retornamos -1
 	if (n == NULL) return -1;
-    return height(n -> izquierdo) - height(n -> derecho);
+	//se retorna la altura del hijo izquierdo menos la altura del hijo derecho
+  return height(n -> izquierdo) - height(n -> derecho);
 }
 Nodo * MapAVL::rightRotate(Nodo * y){
+	//creamos un nodo x que apunte al hijo izquierdo de y
 	Nodo * x = y -> izquierdo;
-    Nodo * T2 = x -> derecho;
-
-    // rotacion  
-    x -> derecho = y;
-    y -> izquierdo = T2;
-
-    return x;
+	//creamos un nodo T2 que sera el auxiliar que apuntara al hijo derecho de x
+  Nodo * T2 = x -> derecho;
+  //comienza la rotacion, hacemos que el hijo derecho de x sea y
+  //y que el hijo izquierdo de y sea T2, de esta forma logramos que x quede a la cabeza
+  //y quede como hijo derecho de x y que T2 quede como hijo izquierdo de y 
+  x -> derecho = y;
+	y -> izquierdo = T2;
+	//retornamos la cabeza de esta rotacion, que seria x
+	return x;
 }
 Nodo * MapAVL::leftRotate(Nodo * x){
+	//creamos un nodo y que apunte al hijo derecho de x
 	Nodo * y = x -> derecho;
-    Nodo * T2 = y -> izquierdo;
+	//creamos un nodo T2 que sera el auxiliar que apuntara al hijo derecho de y
+	Nodo * T2 = y -> izquierdo;
 
-    // rotacion 
-    y -> izquierdo = x;
-    x -> derecho = T2;
-
-    return y;
+  //comienza la rotacion, hacemos que el hijo izquierdo de y sea x
+  //y que el hijo derecho de x sea T2, de esta forma logramos que y quede a la cabeza
+  //x quede como hijo izquierdo de y, y T2 quede como hijo derecho de x
+  y -> izquierdo = x;
+  x -> derecho = T2;
+  //retornamos la cabeza de esta rotacion, que seria y
+	return y;
 }
 int MapAVL::height(Nodo * r){
+		//si estamos en una hoja retornamos -1
     if (r == NULL) return -1;
     else {
-      /* se saca la altura de cada hijo */
+      //se saca la altura de cada hijo 
       int izqHeight = height(r -> izquierdo);
       int derHeight = height(r -> derecho);
 
-      /* usamos el mayor */
+      //retornamos la altura del mayor hijo
       if (izqHeight > derHeight)
         return (izqHeight + 1);
       else return (derHeight + 1);
@@ -202,7 +242,7 @@ Nodo * MapAVL::getRaiz(){
 }
 Nodo * MapAVL::minValueNodo(Nodo * nodo){
     Nodo * current = nodo;
-    /* se busca a la hoja mas externa de la izquierda */
+    //se busca a la hoja mas externa de la izquierda 
     while (current -> izquierdo != NULL) {
       	current = current -> izquierdo;
     }
